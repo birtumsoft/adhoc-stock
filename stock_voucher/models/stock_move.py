89cf63ace242ca8e43464cbf7c81b836aeac8570
Irene Birtum <iic@birtum.com>
2025-11-30 13:15:21 -0300

    des-migration

--- stock_voucher/models/stock_move.py
+++ stock_voucher/models/stock_move.py
@@ -2,8 +2,12 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import stock_book
-from . import stock_picking_type
-from . import stock_picking
-from . import stock_picking_voucher
-from . import stock_move
+from odoo import fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    vouchers = fields.Char(
+        related="picking_id.vouchers",
+    )
